6916304c169bea00c5094b53e0cce9ec136e22c9
Daniel Marchena Parreira <danielmapar@gmail.com>
2013-11-02 11:48:01 -0200

    1.1-Getting Started

---
+++ src/1.1-GettingStarted-1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : 1.1-GettingStarted-1.c
+ Author      : Daniel Marchena Parreira
+ Email       : danielmapar@gmail.com
+ Date        : 31/10/2013
+ Version     : 0.1
+ Copyright   : GNU General Public License Version 2
+ Description : Basic Hello World program
+ ============================================================================
+ */
+
+int main(void)
+{
+	printf("Hello, World");
+
+	return 0;
+}
---
+++ src/1.1-GettingStarted-2.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : 1.1-GettingStarted-2.c
+ Author      : Daniel Marchena Parreira
+ Email       : danielmapar@gmail.com
+ Date        : 31/10/2013
+ Version     : 0.1
+ Copyright   : GNU General Public License Version 2
+ Description : Basic Hello World program v2
+ ============================================================================
+ */
+
+int main(void)
+{
+	printf("Hello, World\n");
+	printf("Good Morning");
+
+	return 0;
+}
